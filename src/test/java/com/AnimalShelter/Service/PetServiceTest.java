4e60228d5ef90cd757febce7edda9003fb2918b9
JMPolvillo <rothkildam@gmail.com>
2024-08-22 14:23:56 +0200

    added some changes

--- src/test/java/com/AnimalShelter/Service/PetServiceTest.java
+++ src/test/java/com/AnimalShelter/Service/PetServiceTest.java
@@ -1,8 +1,14 @@
 package com.AnimalShelter.Service;
 
+import com.AnimalShelter.Model.Pet;
 import com.AnimalShelter.Repository.IPetRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
 
 @SpringBootTest
 public class PetServiceTest {
@@ -10,4 +16,19 @@ public class PetServiceTest {
     @Mock
     private IPetRepository iPetRepository;
 
+    @InjectMocks
+    private PetService petService;
+
+    private Pet pet1;
+    private Pet pet2;
+    private final ArrayList<Pet> petList = new ArrayList<>();
+
+    @BeforeEach
+    public void SetUp(){
+        MockitoAnnotations.openMocks(this);
+
+        pet1 = new Pet();
+        pet1.setId(1);
+        pet1.setName("Bolita");
+    }
 }
